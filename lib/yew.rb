cc2e70f804fb00e212b11a077edcf1b3bad565cd
gian <snmgian@gmail.com>
2014-03-27 22:25:38 -0300

    Document code.

--- lib/yew.rb
+++ lib/yew.rb
@@ -1,20 +1,39 @@
 require 'yaml'
 
 module Yew
+
+  # Creates an object tree based on a given list of yml files.
+  #
+  # @param [*String] Yaml source files
+  #
+  # @return [Yew::Tree]
   def self.load(*yml_files)
     env_hash = yml_files.inject({}) do |memo, yml|
-      memo.merge! YAML.load(File.read(yml))
+      memo.merge!(YAML.load(File.read(yml)))
     end
 
     Tree.new(env_hash)
   end
 
+  # The object tree. Allows root-to-leave navigation in the form of ordinary method calls.
   class Tree < BasicObject
+
+    # @param [Hash] env
+    # @param root Tree's root. Used for notifying a not found entry at an exact path point.
     def initialize(env, root = nil)
       @env = env
       @root = root
     end
 
+    # Fetches a branch or leaf.
+    #
+    # If no key is received, returns the underlying Hash.
+    #
+    # @param key Node's name.
+    #
+    # @return [Yew::Tree] When the request node is still a tree.
+    # @return [Hash] Underlying Hash.
+    # @return [Object] When the requested node is a leaf.
     def [](*key)
       if key.any?
         key = key.first
@@ -29,6 +48,7 @@ module Yew
       Utils.fetch(m, @env, @root)
     end
 
+    # Define methods for printing inspection.
     if $YEW_DEBUG
       def inspect
         "<Envy::Env:#{__id__} -> #@env>"
@@ -41,6 +61,13 @@ module Yew
   end
 
   module Utils
+
+    # Fetches the key's value.
+    #
+    # Returns a tree if the value is a hash. Otherwise it returns the raw value.
+    #
+    # @return [Yew::Tree]
+    # @return [Object]
     def self.fetch(key, env, root)
       value = raw_fetch(key, env, root)
 
@@ -54,6 +81,9 @@ module Yew
       end
     end
 
+    # Fetches a value from the env.
+    #
+    # @raise [RuntimeError] When key not found.
     def self.raw_fetch(key, env, root)
       env.fetch(key) do
         env.fetch(key.to_s) do
